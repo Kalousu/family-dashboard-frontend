ec58a5f0743ddf46fd402442361b1e698d86504a
Katrin Seeberg <katrinseeberg@gmail.com>
2026-04-01 20:46:52 +0200

    some design tweaks

--- apps/dashboard/src/App.js
+++ apps/dashboard/src/App.js
@@ -44,8 +44,12 @@ function Topbar({ onMenuToggle }) {
             </svg>
           )}
         </button>
-        <button className="icon-btn" onClick={onMenuToggle} title="Menu">
-          <span className="burger">&#9776;</span>
+        <button className="icon-btn profile-btn" onClick={onMenuToggle} title="Profil">
+          <div className="avatar">
+            <svg viewBox="0 0 24 24" fill="currentColor" width="18" height="18">
+              <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/>
+            </svg>
+          </div>
         </button>
       </div>
     </header>
