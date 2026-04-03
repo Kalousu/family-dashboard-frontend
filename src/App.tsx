353e6b6637080b84d5d02bf6f7a87df092b7d718
Katrin Seeberg <katrinseeberg@gmail.com>
2026-04-03 21:07:38 +0200

    added profilecard, general files for log in + implemented tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { getWidget } from './widgets/WidgetRegistry'
+import ProfileCard from './components/ProfileCard'
 
 const ComponentWeather = getWidget("weather");
 const ComponentCalendar = getWidget("calendar");
@@ -18,6 +19,7 @@ function App() {
     <ComponentWeather />
     <ComponentWeather />
     <ComponentCalendar />
+    <ProfileCard name="Kevin" color="blue" image="https://cdn.discordapp.com/attachments/887358855372242975/1489658243469938729/image.png?ex=69d137d9&is=69cfe659&hm=967d1f4fcbd31d3c6c357d93754d1ddf2416b11b38a21339e3a6b6c1a810c343&" onSelect={() => alert("Profile selected")} />
     </>
   )
 }
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,21 @@
+interface ProfileCardProps {
+  name: string
+  color: string
+  image?: string
+  onSelect: () => void
+}
+
+function ProfileCard({ name, color, image, onSelect }: ProfileCardProps) {
+    return (
+        <div className="flex flex-col items-center cursor-pointer hover:scale-105 transition-transform"onClick={onSelect}>
+            {image ? (
+                <img src={image} className="w-24 h-24 rounded-full object-cover overflow-hidden" />
+            ) : (
+                <div className="w-24 h-24 rounded-xl" style={{ backgroundColor: color }} />
+            )}
+            <p className="text-lg font-semibold text-gray-800">{name}</p>
+        </div>
+    )
+}
+
+export default ProfileCard
