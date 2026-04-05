65ec17533e7334ca8c7de46ea066511869024a51
Katrin Seeberg <katrinseeberg@gmail.com>
2026-04-05 11:26:24 +0200

    edited pw-input to use enter key

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
+import type { KeyboardEvent } from "react"
 
 function PasswortInput({ onLogin }: { onLogin: (password: string) => void }) {
     const [password, setPassword] = useState("")
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            onLogin(password)
+        }
+    }
     
     return (
         <div className="flex flex-row items-center gap-2">
             <input 
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                onKeyDown={handleKeyDown}
                 className="px-4 py-2 rounded-xl border-2 border-gray-400 focus:outline-none bg-gray-200 text-sm" 
                 type="password" 
                 placeholder="Passwort" />
