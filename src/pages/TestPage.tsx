672c5be723c23d3f564b14cb3b7c10661a065b15
Katrin Seeberg <katrinseeberg@gmail.com>
2026-04-05 13:23:05 +0200

    added containersizer to weatherwidget

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -4,7 +4,7 @@ function TestPage() {
 
     return(
         <div className="min-h-screen flex items-center justify-center p-8" style={{ backgroundImage: "url('https://everwallpaper.com/cdn/shop/products/cartoon-cat-wall-mural-plain.jpg?v=1710129966')", backgroundSize: "cover" }}>
-            <div className = "w-100 h-125">
+            <div className = "w-100 h-135">
                 <WeatherWidget />
             </div>
         </div>
--- src/widgets/weather/WeatherWidget.tsx
+++ src/widgets/weather/WeatherWidget.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react"
 import { MapPin, Wind } from "lucide-react"
+import { useContainerSize } from "../../hooks/useContainerSize"
 
 function WeatherWidget() {
+
+    const { ref, width } = useContainerSize()
+    const isCompact = width < 220
 
     const [weatherData, setWeatherData] = useState({
         temperature: 17,
@@ -11,18 +15,29 @@ function WeatherWidget() {
     })
 
     return(
-        <div className="w-full h-full bg-linear-to-b from-sky-500/30 to-sky-200/30 backdrop-blur-md border border-white/20 rounded-2xl shadow-lg p-4">
-            <div className="flex flex-row items-center gap-2 mb-4">
-                <MapPin color="white" size={32} />
-                <p className="text-white text-center text-4xl font-bold">{weatherData.location}</p>
-            </div> 
-            <div className="flex flex-col items-center justify-center m-6">
-                <p className="text-white text-center text-7xl font-semibold m-8">{weatherData.temperature}°C</p>
-                <div className="flex flex-row items-center gap-2 mb-4">
-                    <Wind color="white" size={32} />
-                    <p className="text-white text-center text-xl font-bold">Wind: {weatherData.windSpeed} km/h</p>
-                </div>            
-            </div>           
+        <div ref={ref} className="w-full h-full bg-linear-to-b from-sky-500/30 to-sky-200/30 backdrop-blur-md border border-white/20 rounded-2xl shadow-lg p-4">
+            { isCompact ? (
+                <div className="flex flex-col items-center justify-center m-4">
+                    <p className="text-white text-center text-3xl font-bold">{weatherData.location}</p>
+                    <p className="text-white text-center text-7xl font-semibold m-3">{weatherData.temperature}°C</p>   
+                    <p className="text-white text-center text-xl font-bold">Wind: {weatherData.windSpeed} km/h</p>      
+                </div> 
+            ) : (
+                <div>
+                    <div className="flex flex-row items-center gap-2 mb-4">
+                        <MapPin color="white" size={32} />
+                        <p className="text-white text-center text-4xl font-bold">{weatherData.location}</p>
+                    </div> 
+                    <div className="flex flex-col items-center justify-center m-6">
+                        <p className="text-white text-center text-7xl font-semibold m-8">{weatherData.temperature}°C</p>
+                        <div className="flex flex-row items-center gap-2 mb-4">
+                            <Wind color="white" size={32} />
+                            <p className="text-white text-center text-xl font-bold">Wind: {weatherData.windSpeed} km/h</p>
+                        </div>
+                    </div>
+                </div> 
+            ) }
+          
         </div>
     )
 }
