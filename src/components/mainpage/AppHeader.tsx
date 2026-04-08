37ae9c2daed4520c123d51d316caf28abbcfd58f
Katrin Seeberg <katrinseeberg@gmail.com>
2026-04-08 11:17:13 +0200

    added appheader

---
+++ src/components/mainpage/AppHeader.tsx
@@ -0,0 +1,16 @@
+import { User } from "lucide-react"
+
+interface AppHeaderProps {
+    onUserClick: () => void
+}
+
+function AppHeader({ onUserClick }:AppHeaderProps) {
+    return (
+        <div className="w-full h-13 bg-linear-90 from-gray-600 to-gray-700 flex flex-row items-center justify-between p-4">
+            <p className="text-white text-center text-2xl font-semibold">Family-Dashboard</p>
+            <User className="w-8 h-8 rounded-full bg-gray-600 text-gray-400 hover:scale-105 hover:bg-gray-500 hover:text-white transition-all" size={20} onClick={onUserClick}/>   
+        </div>
+    )
+}
+
+export default AppHeader
--- src/pages/WidgetPage.tsx
+++ src/pages/WidgetPage.tsx
@@ -1,7 +1,15 @@
+import AppHeader from "../components/mainpage/AppHeader"
+import { useState } from "react"
+
 function WidgetPage() {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+
     return (
-        <div>
-            <p>hello world from widgetpage</p>
+        <div className="flex flex-col h-screen bg-linear-to-b from-gray-400 to-gray-200">
+            <div className="h-screen">
+                <AppHeader onUserClick={() => setSidebarOpen(!sidebarOpen)}/>
+            </div>
         </div>
     )
 }
