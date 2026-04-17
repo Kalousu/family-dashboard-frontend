79c0abb62734a5cc350d8b2f13e8253055a4b625
Katrin Seeberg <katrinseeberg@gmail.com>
2026-04-17 13:31:38 +0200

    hot fix: removed testpages from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import ProfileSelectPage from './pages/ProfileSelectPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import TestPage from './pages/TestWeatherPage';
-import TestTimetablePage from './pages/TestTimetablePage';
-import TestPageToDo from './pages/TestPageToDo';
 import WidgetPage from './pages/WidgetPage';
 import { DarkModeProvider } from './context/DarkModeContext';
 import { UserProvider } from './context/UserContext';
@@ -18,9 +15,6 @@ function App() {
           <Route path="/" element={<ProfileSelectPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/test" element={<TestPage />} />
-          <Route path="/testtimetable" element={<TestTimetablePage />} />
-          <Route path="/test-todo" element={<TestPageToDo />} />
           <Route path="/dashboard" element={<WidgetPage />} />
         </Routes>
       </BrowserRouter>
