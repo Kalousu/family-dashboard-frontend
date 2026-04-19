f2a00ea8f81ad824fb92d6970deb5f1f695d8229
DanielBrecht <s241293@student.dhbw-mannheim.de>
2026-04-19 19:36:28 +0200

    Fehleranzeige in Konsole entfernt

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -4,17 +4,4 @@ const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 })
 
-axiosInstance.interceptors.response.use(
-    response => response,
-    error => {
-        const { config, response } = error;
-        console.error(
-            `[API Error] ${config?.method?.toUpperCase()} ${config?.url}`,
-            `→ Status: ${response?.status}`,
-            `→ Body:`, response?.data
-        );
-        return Promise.reject(error);
-    }
-)
-
 export default axiosInstance
